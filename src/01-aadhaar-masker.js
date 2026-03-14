726b279dd661cffca9ae5c719241e87234b5de80
Rushikesh <rushijadhav1423@gmail.com>
2026-03-14 14:10:29 +0530

    Completed 1 & 2

--- src/01-aadhaar-masker.js
+++ src/01-aadhaar-masker.js
@@ -28,5 +28,13 @@
  *   // => "INVALID"
  */
 export function maskAadhaar(aadhaarNumber) {
-  // Your code here
+  if(typeof aadhaarNumber !== "string") return "INVALID";
+
+  if(aadhaarNumber.length !== 12) return "INVALID";
+
+  if(!/^\d{12}$/.test(aadhaarNumber)) return "INVALID"
+
+  const maskAadhaar = aadhaarNumber.slice(-4);
+
+  return `XXXX-XXXX-${maskAadhaar}`
 }
